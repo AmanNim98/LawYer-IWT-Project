c69036148db5e40b3c738a9ce91c88570be7a417
Sagar Kalra <sagar.kalra98@hotmail.com>
2018-11-20 01:17:33 +0530

    compleye

--- loginSignup/models.py
+++ loginSignup/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+    name = models.CharField(max_length = 100)
+    email = models.EmailField()
+    password = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.name + ":" + self.email
+
+class contactUs(models.Model):
+    name = models.CharField(max_length = 100, null = True, blank = True)
+    phone = models.BigIntegerField( null = True, blank = True)
+    email = models.EmailField( null = True, blank = True)
+    country = models.CharField(max_length =  50, null = True, blank = True)
+    subject = models.TextField(null = True, blank = True)
+
+    def __str__(self):
+        return self.name + ":" + self.email
+
--- loginSignup/views.py
+++ loginSignup/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from .models import *
+from django.contrib import messages
 # Create your views here.
+def LoginView(request):
+    dictV = {}
+    if request.method == 'POST':
+        userLogin = user.objects.get(email = request.POST['username'], password = request.POST['password'])
+        print(userLogin)
+        if (userLogin):
+            messages.success(request, "User Logged in successfully")
+            return render(request, 'index.html')
+        else:
+            messages.error(request,'User not found please recheck your credentials')
+    return render(request, 'login.html', dictV)
+
+def ContactUs(request):
+    if request.method == 'POST':
+        newQuery = contactUs(name = request.POST['name'], phone = request.POST['phone'], email = request.POST['email'], country = request.POST['country'], subject = request.POST['subject'])
+        newQuery.save()
+        messages.success(request, "Your contact information has been successfully saved.")
+        return render(request, 'index.html')
+    return render(request, 'contact-us.html')
